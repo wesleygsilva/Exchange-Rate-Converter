17889037c182d3d10f009eb5cd7b01f133ba5fa3
Wesley Gomes <gomes.wesley@outlook.com.br>
2022-02-15 21:31:17 -0300

    Refactoring script.js file: improvements on functions and readability

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+const API_KEY = '8d478b10-8d29-11ec-9dcf-ab45d70362de';
+
 const firstCurrencyEl = document.querySelector('[data-js="first-currency"]');
 const secondCurrencyEl = document.querySelector('[data-js="second-currency"]');
 const valueToConverter = document.querySelector('[data-js="value-to-converter"]');
@@ -6,11 +8,13 @@ const resultExchangeRate = document.querySelector('[data-js="result"]');
 
 let internalExchangeData = { };
 
-
-const getUrl = currency => `https://freecurrencyapi.net/api/v2/latest?apikey=8d478b10-8d29-11ec-9dcf-ab45d70362de&base_currency=${currency}`;
+const getUrl = currency => `https://freecurrencyapi.net/api/v2/latest?apikey=${API_KEY}&base_currency=${currency}`;
 
 const getErrorMessage = errorType => ({
-//Mensagens de erro
+    '200': 'Successful Request',
+    '429': 'You have hit your rate-limit',
+    '404': 'A requested resource does not exist',
+    '500': 'Internal Server Error - let us know: freecurrencyapidotnet.net'
 })[errorType] || 'Não foi possivel obter as informações';
 
 const fetchExchangeRate = async (url) => {
@@ -21,7 +25,7 @@ const fetchExchangeRate = async (url) => {
         internalExchangeData = {...exchangeRateData}
 
         if (exchangeRateData.data === '') { 
-            throw new Error(getErrorMessage('Não foi possivel obter as informações'));
+            throw new Error(getErrorMessage(exchangeRateData.status));
         }
 
         return exchangeRateData;
@@ -31,46 +35,68 @@ const fetchExchangeRate = async (url) => {
     }
 }
 
-const init = async () => {
-    internalExchangeData = {...(await fetchExchangeRate(getUrl('USD')))};
-
-    const sortArray = Object.keys(internalExchangeData.data);
-    sortArray.push('USD')
-    sortArray.sort();
-
-    const getOptions = selectedCurrency => sortArray
-        .map(currency => `<option ${currency === selectedCurrency ? 'selected' : ''}>${currency}</option>`)
-        .join('');
-
-    // pega taxa inicial
-    firstCurrencyEl.innerHTML = getOptions('USD');
-    secondCurrencyEl.innerHTML = getOptions('BRL'); 
-
-    convertedCurrency.textContent = (valueToConverter.value * internalExchangeData.data[secondCurrencyEl.value]).toFixed(2);
-
-    
-// mostra referencia ao usuário
-resultExchangeRate.textContent = '1 ' + firstCurrencyEl.value + ' = ' + internalExchangeData.data[secondCurrencyEl.value].toFixed(5) + ' ' + secondCurrencyEl.value;
-    
+const showReferenceConvertedCurrency = () => {
+    resultExchangeRate.textContent = 
+        '1 ' + 
+        firstCurrencyEl.value + 
+        ' = ' + 
+        internalExchangeData.data[secondCurrencyEl.value].toFixed(5) + 
+        ' ' + 
+        secondCurrencyEl.value;
 }
 
-//Multiplica o valor de acordo com o input
-valueToConverter.addEventListener('input', e => {
-    convertedCurrency.textContent = (e.target.value * internalExchangeData.data[secondCurrencyEl.value]).toFixed(2);
-})
+const showConvertedCurrency = () => {
+    convertedCurrency.textContent = 
+        (valueToConverter.value * internalExchangeData.data[secondCurrencyEl.value]).toFixed(2);   
+}
 
+const addCurrencyonCurrencys = CurrentCurrency => {
+    const Currency = Object.keys(internalExchangeData.data);
+    Currency.push(CurrentCurrency);
+    Currency.sort();
+
+    return Currency;
+}
+
+const getOptions = (selectedCurrency, Currencys) => {
+    const setSelectedAttribute = currency => 
+        currency === selectedCurrency ? 'selected' : '';
+
+    return Currencys
+        .map(currency => `<option ${setSelectedAttribute(currency)}>${currency}</option>`)
+        .join('');
+}
+
+const setInitialsCurrencys = Currencys => {
+    firstCurrencyEl.innerHTML = getOptions('USD', Currencys);
+    secondCurrencyEl.innerHTML = getOptions('BRL', Currencys); 
+}
+
+const showInitialInfo = Currencys => {
+    setInitialsCurrencys(Currencys);
+    showConvertedCurrency();
+    showReferenceConvertedCurrency();
+}
+
+valueToConverter.addEventListener('input', e => {
+    showConvertedCurrency();
+})
 
 secondCurrencyEl.addEventListener('input', e => {
-    convertedCurrency.textContent = (valueToConverter.value * internalExchangeData.data[secondCurrencyEl.value]).toFixed(2);
-    resultExchangeRate.textContent = '1 ' + firstCurrencyEl.value + ' = ' + internalExchangeData.data[secondCurrencyEl.value].toFixed(5) + ' ' + secondCurrencyEl.value;
+    showConvertedCurrency();
+    showReferenceConvertedCurrency();
 })
 
-firstCurrencyEl.addEventListener('input',async e => {
+firstCurrencyEl.addEventListener('input', async e => {
     internalExchangeData = {...(await fetchExchangeRate(getUrl(e.target.value)))};
-    convertedCurrency.textContent = (valueToConverter.value * internalExchangeData.data[secondCurrencyEl.value]).toFixed(2);
-    resultExchangeRate.textContent = '1 ' + firstCurrencyEl.value + ' = ' + internalExchangeData.data[secondCurrencyEl.value].toFixed(5) + ' ' + secondCurrencyEl.value;
+    showConvertedCurrency();
+    showReferenceConvertedCurrency();
 })
 
-
+const init = async () => {
+    internalExchangeData = {...(await fetchExchangeRate(getUrl('USD')))};
+    const Currencys = addCurrencyonCurrencys('USD');
+    showInitialInfo(Currencys);
+}
 
 init();
